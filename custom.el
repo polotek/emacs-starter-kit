cb63f0cd8e8cc4257bd9eff60dba113fe910780d
marogers <marogers@yammer-inc.com>
2011-06-24 17:15:43 -0700

    change perms on custom.el

---
+++ custom.el
@@ -0,0 +1,51 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(backup-by-copying nil)
+ '(backup-by-copying-when-linked t)
+ '(backup-by-copying-when-mismatch t)
+ '(backup-directory-alist (quote (("." . "~/.emacs.d/backups"))))
+ '(blink-matching-delay 3)
+ '(c-basic-offset 4)
+ '(c-default-style (quote ((objc-mode . "") (java-mode . "java") (awk-mode . "awk") (other . "gnu"))))
+ '(column-number-mode t)
+ '(css-indent-level 4)
+ '(espresso-enabled-frameworks (quote (javascript mochikit prototype dojo extjs merrillpress)))
+ '(espresso-expr-indent-offset 2)
+ '(espresso-indent-level 2)
+ '(js2-allow-keywords-as-property-names nil)
+ '(js2-auto-indent-flag nil)
+ '(js2-auto-insert-catch-block nil)
+ '(js2-basic-offset 2)
+ '(js2-bounce-indent-flag t)
+ '(js2-bounce-indent-p t)
+ '(js2-cleanup-whitespace nil)
+ '(js2-electric-keys (quote nil))
+ '(js2-enter-indents-newline nil)
+ '(js2-highlight-external-variables t)
+ '(js2-highlight-level 3)
+ '(js2-idle-timer-delay 0.4)
+ '(js2-mirror-mode nil)
+ '(js2-mode-escape-quotes t)
+ '(js2-mode-show-parse-errors t)
+ '(js2-strict-missing-semi-warning nil)
+ '(nxml-child-indent 4)
+ '(nxml-outline-child-indent 4)
+ '(paren-match-face (quote paren-face-match-light))
+ '(paren-sexp-mode t)
+ '(show-paren-mode t)
+ '(show-paren-style (quote parenthesis))
+ '(smarty-electric-mode nil)
+ '(smarty-intelligent-tab nil)
+ '(smarty-stutter-mode nil)
+ '(tramp-auto-save-directory "~/.emacs.d/auto_saves")
+ '(tramp-copy-size-limit 40960)
+ '(tramp-encoding-shell "/bin/bash"))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(mumamo-background-chunk-submode1 ((((class color) (min-colors 88) (background dark)) (:background "q")))))
